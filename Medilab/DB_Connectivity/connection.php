8379d38b2df83e9b8d769465816184fbf0297a2d
Khwaja-Khunshan <125073022+Khwaja-Khunshan@users.noreply.github.com>
2023-02-11 00:15:48 -0800

    Delete Medilab directory

--- Medilab/DB_Connectivity/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $db_name = "corona_db";
-    
-    // Connection
-    $conn = mysqli_connect($servername,$username, $password,$db_name); 
-?>
